268328cfd9f179c7a0751f84cdc7be87eff05424
derekdorey <derekdorey@ME4233-09.labs.sce.carleton.ca>
2016-12-04 19:14:31 -0500

    wrestling with git

---
+++ src/com/sysc/tftp/utils/TestLogger.java
@@ -0,0 +1,106 @@
+package com.sysc.tftp.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import com.sysc.tftp.client.Client;
+import com.sysc.tftp.server.Server;
+
+public class TestLogger {
+	
+	private ArrayList<String> clientSendLog;
+	private ArrayList<String> clientReceiveLog;
+	private ArrayList<String> serverSendLog;
+	private ArrayList<String> serverReceiveLog;
+	private boolean isClientLog;
+	
+	
+	public TestLogger(Client user) {
+		
+		clientSendLog = new ArrayList<String>();
+		clientReceiveLog = new ArrayList<String>();
+		isClientLog = true;
+	}
+	
+	public TestLogger(Server server) {
+		
+		serverSendLog = new ArrayList<String>();
+		serverReceiveLog = new ArrayList<String>();
+		isClientLog = false;
+	}
+	
+	public void archive(boolean sending, DatagramPacket packet) {
+		
+		byte[] data = new byte[Variables.MAX_PACKET_SIZE];
+		byte[] blockNumber = new byte[2];
+		
+		data = Arrays.copyOfRange(packet.getData(), 0, packet.getLength());
+		blockNumber[0] = data[2];
+		blockNumber[1] = data[3];
+		
+		String type;
+		
+		if(data[0]!=0) {
+			type = "INVALID";
+		}
+		
+		if(data[1]==1) {
+			type = "RRQ";
+			//blockNumber = BlockUtil.intToByte(-1);  //encode as -1 for querying
+		} else if(data[1]==2) {
+			type = "WRQ";
+			//blockNumber = BlockUtil.intToByte(-1);  //encode as -1 for querying
+		} else if(data[1]==3) {
+			type = "DATA";
+		} else if(data[1]==4) {
+			type = "ACK";
+		} else if(data[1]==5) {
+			type = "ERROR";
+		} else {
+			type = "INVALID"; 		
+		}
+		
+		if(isClientLog && sending) { //if the caller is a Client sending a data packet
+			
+			clientSendLog.add(type);
+			clientSendLog.add(String.valueOf(BlockUtil.byteToInt(blockNumber)));
+			
+		} else if (isClientLog) {  //caller is a Client receiving a data packet
+			
+			clientReceiveLog.add(type);
+			clientReceiveLog.add(String.valueOf(BlockUtil.byteToInt(blockNumber)));
+			
+		} else if(!(isClientLog) && sending) { //caller is a Server thread sending a data packet
+			
+			serverSendLog.add(type);
+			serverSendLog.add(String.valueOf(BlockUtil.byteToInt(blockNumber)));
+			
+		} else { //else the caller is a Server Thread receiving a data packet
+		
+			serverReceiveLog.add(type);
+			serverReceiveLog.add(String.valueOf(BlockUtil.byteToInt(blockNumber)));
+			
+		}
+	}
+
+	public ArrayList<String> getClientSendLog() {
+		return clientSendLog;
+	}
+
+	public ArrayList<String> getClientReceiveLog() {
+		return clientReceiveLog;
+	}
+
+	public ArrayList<String> getServerSendLog() {
+		return serverSendLog;
+	}
+
+	public ArrayList<String> getServerReceiveLog() {
+		return serverReceiveLog;
+	}
+	
+	
+	
+}
