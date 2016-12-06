997a7d4468a8d160762ed8704979e62dd44ae628
scotthanton <scott_hanton94@hotmail.com>
2016-12-06 13:57:45 -0500

    Fix block numbers being considered signed, now unsigned and transfers up to ~65500 blocks.

--- src/com/sysc/tftp/utils/BlockUtil.java
+++ src/com/sysc/tftp/utils/BlockUtil.java
@@ -1,34 +1,32 @@
 package com.sysc.tftp.utils;
 
-import java.math.BigInteger;
-
 public class BlockUtil {
 
 	public static byte[] intToByte(int i) {
-	    BigInteger bigInt = BigInteger.valueOf(i);      
-	    byte[] b = bigInt.toByteArray();
-	    
-	    if (b.length == 1) {
-	    	byte[] b2 = new byte[2];
-	    	b2[0] = 0;
-	    	b2[1] = b[0];
-	    	return b2;
-	    }
+		
+		byte[] b = new byte[2];
+		b[0] = (byte) (i >> 8);
+		b[1] = (byte) (i);
+		
 	    return b;
 	}
+
 	
 	public static int byteToInt(byte[] bytes) {
-		return new BigInteger(1, bytes).intValue();
+		return ((bytes[0] << 8) & 0xFF00) | (bytes[1] & 0xFF);
 	}
 
-	@Deprecated
-	public static int oldConversion(byte[] data) {
-		return ((data[2] & 0xff) << 8) | (data[3] & 0xff);
-	}
 	
-	@Deprecated
-	public static int oldConversion2(byte[] received) {
-		return ((received[2] << 8) & 0xFF00) | (received[3] & 0xFF);
+	public static void main(String[] args) {
+
+		int test = 60000;
+		System.out.println("Integer: " + test);
+		byte[] b = intToByte(test);
+		for (byte b2 : b) {
+		    System.out.println(Integer.toBinaryString(b2 & 255 | 256).substring(1));
+		}
+		System.out.println("To Bytes: " + b);
+		System.out.println("Back to integer " + byteToInt(b));
+		
 	}
-	
 }
